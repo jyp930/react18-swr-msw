05fe2360e6c53c53946745fbcf8097696d245ff1
jyp0808 <jyp0808@loplat.com>
2022-04-28 19:10:09 +0900

    useDeferredValue

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Individual from './components/Individual';
 import Integration from './components/Integration';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Individual />
       <hr />
       <Integration />
+      <hr />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useDeferredValue, useMemo } from 'react';
+
+function Counter() {
+  const [value, setValue] = useState('');
+  const deferredValue = useDeferredValue(value);
+
+  const boxes = useMemo(() => {
+    return (
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {new Array(10000).fill(null).map(() => {
+          const x = Math.floor(Math.random() * 256);
+          const y = Math.floor(Math.random() * 256);
+          const z = Math.floor(Math.random() * 256);
+          const backgroundColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+          return <div style={{ width: 100, height: 100, backgroundColor }}>{deferredValue[0]}</div>;
+        })}
+      </div>
+    );
+  }, [deferredValue]);
+
+  return (
+    <div>
+      <input onChange={(e) => setValue(e.target.value)} style={{ height: 100 }} />
+      {boxes}
+    </div>
+  );
+}
+export default Counter;
