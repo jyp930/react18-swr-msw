7c19f76f052b5ffda064099b91e8968f30026a31
jyp0808 <jyp0808@loplat.com>
2022-06-16 14:10:50 +0900

    console error 수정

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,12 +7,16 @@ function Counter() {
   const boxes = useMemo(() => {
     return (
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {new Array(10000).fill(null).map(() => {
+        {new Array(10000).fill(null).map((_, index) => {
           const x = Math.floor(Math.random() * 256);
           const y = Math.floor(Math.random() * 256);
           const z = Math.floor(Math.random() * 256);
           const backgroundColor = 'rgb(' + x + ',' + y + ',' + z + ')';
-          return <div style={{ width: 100, height: 100, backgroundColor }}>{deferredValue[0]}</div>;
+          return (
+            <div style={{ width: 100, height: 100, backgroundColor }} key={index}>
+              {deferredValue[deferredValue.length - 1]}
+            </div>
+          );
         })}
       </div>
     );
